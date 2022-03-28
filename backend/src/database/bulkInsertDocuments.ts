f9c9a8bfad637483d6c2bd673d49a498e5f42183
olgasa <olgasa@stud.ntnu.no>
2022-03-28 08:04:15 +0200

    Upload fix

--- backend/src/database/bulkInsertDocuments.ts
+++ backend/src/database/bulkInsertDocuments.ts
@@ -5,11 +5,25 @@ import login from "./login";
 import bulkInsertDocuments from "./queries/bulkInsertDocuments";
 
 export default async (documents: Document[], relations: DocumentSDGRelation[]): Promise<any> => {
-    const query = bulkInsertDocuments(documents, relations);
     await login();
-    
-    return DB.update(query, { transform: 'toJSON' }).catch((err) => {
-        console.log(err);
-    });
+
+    var old_i = 0
+    for (let i = 143; i < documents.length; i += 144) {
+        const query = bulkInsertDocuments(documents.slice(old_i, i), relations); 
+            await DB.update(query, { transform: 'toJSON' })
+            .catch((err) => {
+                console.log(err);
+            });
+/*         documents.slice(old_i, i).forEach(async document => {
+            const query = bulkInsertDocuments([document], relations); 
+            
+            await DB.update(query, { transform: 'toJSON' })
+            .catch((err) => {
+                console.log(err);
+            });
+             */
+        //});
+    old_i = i;
+}
 };
     
