a54c2c7107f9da49c26f8ec34d75746c8f6f9d54
Erik Peralta Løvaas <erikpl@protonmail.com>
2022-03-29 15:19:54 +0200

    small change

--- backend/src/database/getDocumentsForSubgoal.ts
+++ backend/src/database/getDocumentsForSubgoal.ts
@@ -7,7 +7,7 @@ import login from './login';
 
 // export default async (subgoalURI: string, languageCodes: Array<string>, pageNumber: number): Promise<Array<Document>> => {
 export default async (subgoalURI: string): Promise<Array<Document>> => {
-    // const query = getDocumentsForSubgoal(subgoalURI, languageCodes);
+    // const query = getDocumentsForSubgoal(subgoalURI, languageCodes, pageNumber);
     const query = getDocumentsForSubgoal(subgoalURI);
 
     return DB.query(query, { transform: 'toJSON' }).then((resp) => resp.records);
