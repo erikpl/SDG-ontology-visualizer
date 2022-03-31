c6411ac3e8bac8800118c7f2764768a6dbd0d680
Erik Peralta Løvaas <erikpl@protonmail.com>
2022-03-31 10:00:44 +0200

    Created prioritized and celex AND language grouped return array

--- backend/src/routes/ontologies.ts
+++ backend/src/routes/ontologies.ts
@@ -14,7 +14,7 @@ import {
   AnnotationResponse,
   AnyResponse,
   ClassIdRequest,
-  DocumentArrayResponse,
+  DocumentArrayArrayResponse,
   EmptyRequest,
   NodeArrayResponse,
   OntologyArrayResponse,
@@ -24,6 +24,7 @@ import onError from './middleware/onError';
 import verifyDatabaseAccess from './middleware/verifyDatabaseAccess';
 import getDocumentsForSubgoal from '../database/getDocumentsForSubgoal';
 import { Document } from 'types/documentTypes';
+import { doc } from 'prettier';
 
 const router = Router();
 
@@ -120,33 +121,54 @@ const checkMunicipalityByCode = async (req: Request, res: AnyResponse) => {
   }
 };
 
-const getDocumentsForSubgoalByClassId = async (req: Request, res: DocumentArrayResponse) => {
+// TODO: error handling?
+// TODO: move into its own function
+const getDocumentsForSubgoalByClassId = async (req: Request, res: DocumentArrayArrayResponse) => {
   try {
     const langCodes = req.params.langCodes.split(',');
     // unary + converts to number
     // Assuming languageCodes will be sent as a part of the request body. Change if necessary
     // const data = await getDocumentsForSubgoal(req.params.classId, req.body.languageCodes, +req.params.pageNumber);
 
-    const data = await getDocumentsForSubgoal(req.params.classId, langCodes, +req.params.offset);
+    let data = await getDocumentsForSubgoal(req.params.classId, langCodes, +req.params.offset);
    
-    // Documents are sorted into arrays of documents by celexID
-    // The internal arrays are sorted by language, alphabetically (I think)
-    var documentsArray: Document[][] = [];
-    var celexIds = new Set(data.map(doc => doc.celexID));
-    celexIds.forEach(id => {
-      documentsArray.push(data.filter(doc => doc.celexID == id));
-    });
-
-    console.log(documentsArray);
-    // TODO: fjern når vi finner språkkoden på vanlig vis
-    // Trur ikje vi ork ^ går treigar
-    // Replace the language URL with the language code
     for (let i = 0; i < data.length; i++) {
       // The language code is the 7th element when splitting the URL on forward slashes
       data[i]['language'] = data[i]['language'].split('/')[6];
     }
+
     
-    res.json(documentsArray);
+    // Documents are sorted into arrays of documents by celexID
+    // The internal arrays are sorted by language, alphabetically (I think)
+    
+    let documentsArray: Document[][] = [];
+    // To handle edge case that one or more queried language isnt present at all
+    let celexIds = new Set(data.map(doc => doc.celexID));
+
+    // Return array
+    let langGroupedArray: Document[][][] = [];
+   
+    // Create celex-grouped temp array
+    celexIds.forEach(id => {
+      documentsArray.push(data.filter(doc => doc.celexID == id));
+    });
+    
+    // Create prioritized language groupings within each celex group
+    for (let celexIndex = 0; celexIndex < documentsArray.length; celexIndex++) {
+      let celexGroup: Document[][] = [];
+
+      // Create language groupings by filtering and pushing if the language exists for the given document
+      for (let langIndex = 0; langIndex < langCodes.length; langIndex++) {
+        let langMatches = documentsArray[celexIndex].filter(doc => doc.language == langCodes[langIndex]);
+        if (langMatches.length != 0) celexGroup.push(langMatches);
+      }
+
+      // Build up the new array of arrays (of arrays)
+      langGroupedArray.push(celexGroup)
+    }
+
+    res.json(langGroupedArray);
+
   } catch (e: any) {
     onError(e, req, res);
   }
